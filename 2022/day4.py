f2ba5a944645f23b9cabdee574f4885db82008e6
AlexTheJPEG <alexthejpeg@gmail.com>
2023-04-18 23:42:10 +0000

    (2022 Day 4) Use input file instead of `input()`

--- 2022/day4.py
+++ 2022/day4.py
@@ -1,16 +1,14 @@
 contained_ranges = 0
 overlapped_ranges = 0
-while True:
-    try:
-        first, second = [i.split('-') for i in input().split(',')]
+with open("input4.txt", "r") as file:
+    for line in file.read().splitlines():
+        first, second = [i.split('-') for i in line.split(',')]
         first_range = range(int(first[0]), int(first[1]) + 1)
         second_range = range(int(second[0]), int(second[1]) + 1)
         if any([i in first_range for i in second_range]) or any([i in second_range for i in first_range]):
             overlapped_ranges += 1
             if all([i in first_range for i in second_range]) or all([i in second_range for i in first_range]):
                 contained_ranges += 1
-    except EOFError:
-        break
 
-print(f"Completely contained: {contained_ranges}")
-print(f"Overlapping ranges:   {overlapped_ranges}")
+print(contained_ranges)
+print(overlapped_ranges)
