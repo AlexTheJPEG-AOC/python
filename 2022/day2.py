9f92ca25b11199a8c83b2f2c13ab85b234e2e5a6
AlexTheJPEG <alexthejpeg@gmail.com>
2023-04-18 02:18:12 +0000

    (2022 Day 2) Use input file instead of `input()`, combine both parts

--- 2022/day2.py
+++ 2022/day2.py
@@ -1,48 +1,36 @@
-player_score = 0
+part1_score = 0
+part2_score = 0
 
-# Part 1
-"""
 values = {'A': 1,
           'B': 2,
           'C': 3,
           'X': 1,
           'Y': 2,
           'Z': 3}
-while True:
-    try:
-        game = input().split()
-        match game:
-            case ['A', 'Z'] | ['B', 'X'] | ['C', 'Y']:
-                player_score += values[game[1]]
-            case ['A', 'Y'] | ['B', 'Z'] | ['C', 'X']:
-                player_score += values[game[1]] + 6
-            case ['A', 'X'] | ['B', 'Y'] | ['C', 'Z']:
-                player_score += values[game[1]] + 3
-    except EOFError:
-        break
-"""
-
-# Part 2
-lose = {'A': 'C',
+part2_lose = {'A': 'C',
         'B': 'A',
         'C': 'B'}
-win = {'A': 'B',
+part2_win = {'A': 'B',
        'B': 'C',
        'C': 'A'}
-values = {'A': 1,
-          'B': 2,
-          'C': 3}
-while True:
-    try:
-        game = input().split()
+
+with open("input2.txt", "r") as file:
+    for line in file.read().splitlines():
+        game = line.split()
+        match game:
+            case ['A', 'Z'] | ['B', 'X'] | ['C', 'Y']:
+                part1_score += values[game[1]]
+            case ['A', 'Y'] | ['B', 'Z'] | ['C', 'X']:
+                part1_score += values[game[1]] + 6
+            case ['A', 'X'] | ['B', 'Y'] | ['C', 'Z']:
+                part1_score += values[game[1]] + 3
         match game[1]:
             case 'X':  # Need to lose
-                player_score += values[lose[game[0]]]
+                part2_score += values[part2_lose[game[0]]]
             case 'Y':  # Need to draw
-                player_score += values[game[0]] + 3
+                part2_score += values[game[0]] + 3
             case 'Z':  # Need to win
-                player_score += values[win[game[0]]] + 6
-    except EOFError:
-        break
+                part2_score += values[part2_win[game[0]]] + 6
 
-print(player_score)
+print(part1_score)
+print(part2_score)
