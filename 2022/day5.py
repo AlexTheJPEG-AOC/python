24ace77424eefd08afb9be4ab21be6782c196029
AlexTheJPEG <alexthejpeg@gmail.com>
2023-04-18 23:42:27 +0000

    (2022 Day 5) Use input file instead of `input()`, combine both parts

--- 2022/day5.py
+++ 2022/day5.py
@@ -1,45 +1,42 @@
-def split_into_fours(s):
+def split_into_crates(s):
     l = []
     for char in range(0, len(s), 4):
-        l.append(s[char : char + 4].strip())
+        l.append(s[char : char + 4].strip("[] "))
     return l
 
-# Get lines
+
 crates = []
-while True:
-    line = input()
-    if not line:
-        break
-    crates.append(split_into_fours(line))
-del crates[-1]
+with open("input5.txt", "r") as file:
+    lines = file.read().splitlines()
+
+blank_line = lines.index("")
+crate_diagram = lines[:blank_line]
+instructions = lines[blank_line+1:]
+# Get lines
+for line in crate_diagram:
+    crates.append(split_into_crates(line))
 
 # Create stacks
-stacks = []
+part1_stacks = []
+part2_stacks = []
 for index in range(len(crates[0])):
     stack = [row[index] for row in crates if row[index] != '']
     stack.reverse()
-    stacks.append(stack)
+    part1_stacks.append(stack)
+    part2_stacks.append(stack.copy())
 
 # Do move operations
-while True:
-    try:
-        op = input().split()
-        m, f, t = [int(i) for i in op[1::2]]
+for line in instructions:
+    op = line.split()
+    m, f, t = [int(i) for i in op[1::2]]
 
-        # Part 1
-        # for _ in range(m):
-        #    stacks[t - 1].append(stacks[f - 1].pop())
+    # Part 1
+    for _ in range(m):
+       part1_stacks[t - 1].append(part1_stacks[f - 1].pop())
 
-        # Part 2
-        stacks[t - 1].extend(stacks[f - 1][-m:])
-        del stacks[f - 1][-m:]
+    # Part 2
+    part2_stacks[t - 1].extend(part2_stacks[f - 1][-m:])
+    del part2_stacks[f - 1][-m:]
 
-    except EOFError:
-        break
-
-for s in stacks:
-    print(s)
-print()
-
-tops = [stack[-1] for stack in stacks]
-print(tops)
+print("".join([stack[-1] for stack in part1_stacks]))
+print("".join([stack[-1] for stack in part2_stacks]))
