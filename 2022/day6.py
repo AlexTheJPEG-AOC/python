e2d8670b4173764586b2a4bc46805e04cb1273bd
AlexTheJPEG <alexthejpeg@gmail.com>
2023-04-19 00:22:46 +0000

    (2022 Day 6) Use input file instead of `input()`, combine both parts

--- 2022/day6.py
+++ 2022/day6.py
@@ -1,9 +1,13 @@
-stream = input()
+with open("input6.txt", "r") as file:
+    stream = file.read()
 
-# Values: 
-# Packet: 4
-# Message: 14
-for i in range(len(stream) - 14):
-    if sorted(list(stream[i:i+14])) == sorted(list(set(stream[i:i+14]))):
-        print(f"First marker after character {i+14}")
+packets = [stream[i:i+4] for i in range(len(stream) - 3)]
+messages = [stream[i:i+14] for i in range(len(stream) - 13)]
+for index, packet in enumerate(packets):
+    if sorted(list(packet)) == sorted(list(set(packet))):
+        print(index + 4)
+        break
+for index, message in enumerate(messages):
+    if sorted(list(message)) == sorted(list(set(message))):
+        print(index + 14)
         break
