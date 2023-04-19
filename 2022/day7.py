c2ee0c11dfd87a8af2e3c95bc669e07ee34ec1f8
AlexTheJPEG <alexthejpeg@gmail.com>
2023-04-19 00:31:34 +0000

    (2022 Day 7) Use input file instead of `input()`

--- 2022/day7.py
+++ 2022/day7.py
@@ -1,36 +1,34 @@
-current_dir = input().strip("$ cd")
+with open("input7.txt", "r") as file:
+    cmd = file.read().splitlines()
+
+current_dir = cmd[0].strip("$ cd")
 
 ls = []
 full_path = current_dir
 dirs = {}
 listing = False
-while True:
-    try:
-        line = input()
-        if listing:
-            if line[0] == '$':
-                listing = False
-                dirs[full_path] = ls
-                ls = []
-            else:
-                ls.append(line)
-        if line == "$ ls":
-            listing = True
-        elif line.startswith("$ cd "):
-            if line.endswith(".."):
-                current_dir = full_path.split('/')[-2]
-                full_path = full_path[:-(len(current_dir) + 1)]
-                current_dir = full_path.split('/')[-2]
-                # current_dir = full_path.rsplit('/', 1)[0].strip('/')
-                if not current_dir:
-                    current_dir = '/'
-            else:
-                current_dir = line[5:]
-                full_path += f"{current_dir}/"
-    except EOFError:
-        if ls:
+for line in cmd[1:]:
+    if listing:
+        if line[0] == '$':
+            listing = False
             dirs[full_path] = ls
-        break
+            ls = []
+        else:
+            ls.append(line)
+    if line == "$ ls":
+        listing = True
+    elif line.startswith("$ cd "):
+        if line.endswith(".."):
+            current_dir = full_path.split('/')[-2]
+            full_path = full_path[:-(len(current_dir) + 1)]
+            current_dir = full_path.split('/')[-2]
+            if not current_dir:
+                current_dir = '/'
+        else:
+            current_dir = line[5:]
+            full_path += f"{current_dir}/"
+if ls:
+    dirs[full_path] = ls
 
 def size_of(dirs, dirname):
     size = 0
@@ -43,11 +41,9 @@ def size_of(dirs, dirname):
 
 sizes = [] 
 for k in dirs.keys():
-    # print(k, dirs[k])
     sizes.append((k, size_of(dirs, k)))
 
 small_dirs = [d for d in sizes if d[1] <= 100000]
-# print(small_dirs)
 print(sum([d[1] for d in small_dirs]))
 
 outermost_size = sizes[0][1]
